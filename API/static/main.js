faa146bbc00f6755b61d95fe5734b769baeb19fd
David Joy <DavidJoy022@gmail.com>
2022-02-13 05:03:28 -0600

    more setup work

--- API/static/main.js
+++ API/static/main.js
@@ -4,19 +4,46 @@ const render = function() {
 	const rowHTML = ["a","b","c","d","e","f"].map(c => `<div class="square" data-piece="" data-col="${c}"></div>`).join("")
 	document.querySelector("#board").innerHTML = rows.map(i => `<div id="row${i}" class="row">${rowHTML}</div>`).join("");
 
-	document.querySelectorAll("#row2 > .square").forEach(e => e.setAttribute("data-piece", "white-squirrel"));
-	document.querySelectorAll("#row5 > .square").forEach(e => e.setAttribute("data-piece", "black-squirrel"));
+	document.querySelectorAll("#row2 > .square").forEach(e => {
+		e.setAttribute("data-piece", "white-ant")
+		if (color==="white")
+			e.setAttribute("onclick", "upgrade_click(this);")
+	});
+	document.querySelectorAll("#row5 > .square").forEach(e => {
+		e.setAttribute("data-piece", "black-ant");
+		if (color==="black")
+			e.setAttribute("onclick", "upgrade_click(this);")
+	});
+	backrank = document.querySelectorAll(`#row${color==="white" ? "1" : "6"} > .square`);
+	backrank.forEach(e => e.setAttribute("onclick", "empty_square_click(this);"))
 
 	const toPlaceHTML = ["turtle", "bee", "bee", "rabbit", "rabbit", "mouse"].map(a => `<div class="square" data-piece="%color%-${a}" onclick="%onclick%"></div>`).join("");
 	document.querySelector("#self > .piece-pane").innerHTML = toPlaceHTML.replaceAll("%color%", color).replaceAll("%onclick%", "unplaced_piece_click(this);");
 	document.querySelector("#enemy > .piece-pane").innerHTML = toPlaceHTML.replaceAll("%color%", color === "white" ? "black" : "white").replaceAll("%onclick%", "");
 }
-const upgrade_click = function() {
-
+let cur_selected = undefined;
+let backrank = undefined;
+const select = function(elem) {
+	if (cur_selected)
+		cur_selected.classList.remove("selected");
+	elem.classList.add("selected");
+	cur_selected = elem;
+}
+const upgrade_click = function(elem) {
+	select(elem);
+	document.querySelectorAll(`#row${color==="white" ? "1" : "6"} > .square`).forEach(e => {
+		e.classList.remove("highlight");
+	});
 }
 const unplaced_piece_click = function(elem) {
-	elem.classList.add("selected");
-	document.querySelectorAll(" > ").forEach(e => {
-
-	});
+	select(elem);
+	backrank.forEach(e => e.getAttribute("data-piece") || e.classList.add("highlight"));
+}
+const empty_square_click = function(elem) {
+	if (cur_selected && cur_selected.parentElement.classList.contains("piece-pane")) {
+		elem.setAttribute("data-piece", cur_selected.getAttribute("data-piece"));
+		cur_selected.remove();
+		cur_selected = undefined;
+		backrank.forEach(e => e.classList.remove("highlight"));
+	}
 }
