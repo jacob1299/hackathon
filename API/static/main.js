9dd87a65535f93e61a0ca8fe6f5914fc1d48ebbf
David Joy <DavidJoy022@gmail.com>
2022-02-13 03:25:04 -0600

    Progress

--- API/static/main.js
+++ API/static/main.js
@@ -6,6 +6,10 @@ const render = function() {
 
 	document.querySelectorAll("#row2 > .square").forEach(e => e.setAttribute("data-piece", "white-squirrel"));
 	document.querySelectorAll("#row5 > .square").forEach(e => e.setAttribute("data-piece", "black-squirrel"));
+
+	const toPlaceHTML = ["turtle", "bee", "bee", "rabbit", "rabbit", "mouse"].map(a => `<div class="square" data-piece="%color%-${a}" onclick="%onclick%"></div>`).join("");
+	document.querySelector("#self > .piece-pane").innerHTML = toPlaceHTML.replaceAll("%color%", color).replaceAll("%onclick%", "unplaced_piece_click(this);");
+	document.querySelector("#enemy > .piece-pane").innerHTML = toPlaceHTML.replaceAll("%color%", color === "white" ? "black" : "white").replaceAll("%onclick%", "");
 }
 const upgrade_click = function() {
 
