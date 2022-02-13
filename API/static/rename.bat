3d9c5ac700fe4a437a18a2d02c05bf7525765781
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-02-13 02:05:14 -0600

    fixed naming for sprites

--- API/static/rename.bat
+++ API/static/rename.bat
@@ -1,2 +1,4 @@
-for filename in *; do echo mv \"$filename\" \"${filename//icons8-/white_}\"; done | /bin/bash
-for filename in *; do echo mv \"$filename\" \"${filename//-100/}\"; done | /bin/bash
+# for filename in *; do echo mv \"$filename\" \"${filename//-animal/}\"; done > result.txt # | /bin/bash
+for filename in *; do echo mv \"$filename\" \"${filename//-animal/}\"; done | /bin/bash
+
+# for filename in *; do echo mv \"$filename\" \"${filename//-100/}\"; done | /bin/bash
